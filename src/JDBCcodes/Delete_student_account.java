d4c4c2e97f5a817bf7f0a0c91799cd9b63c9e80e
rajil kv <rajilkv@live.com>
2014-10-19 10:53:36 +0530

    no message

---
+++ src/JDBCcodes/Delete_student_account.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package JDBCcodes;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import request.MyModel;
+import request.MyModel_1;
+
+/**
+ *
+ * @author RAJIL KV
+ */
+public class Delete_student_account {
+
+    public Delete_student_account()
+    {
+        
+  String host="jdbc:mysql://localhost:3306/requestdb?zeroDateTimeBehavior=convertToNull";
+        String uName="root";
+        String uPass="";
+        String deleteqry="delete from studentsignup "+ "WHERE userid=?";
+        PreparedStatement ps1=null;
+        try {
+                      Class.forName("com.mysql.jdbc.Driver");
+                      Connection con=DriverManager.getConnection(host,uName,uPass);
+                      ps1=con.prepareStatement(deleteqry);
+                      ps1.setInt(1,MyModel_1.idno);
+                      ps1.executeUpdate();
+                         con.close();
+                     
+                      
+                    }
+            catch (Exception er ) {
+            er.printStackTrace();
+            } 
+    }
+    
+}
