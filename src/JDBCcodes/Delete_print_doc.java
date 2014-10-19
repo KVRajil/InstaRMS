5fdc95ca8e906094b8ab0b977a9ab8d1dc346624
rajil kv <rajilkv@live.com>
2014-10-19 10:50:10 +0530

    no message

---
+++ src/JDBCcodes/Delete_print_doc.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package JDBCcodes;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import request.MyModel_6;
+
+/**
+ *
+ * @author RAJIL KV
+ */
+public class Delete_print_doc {
+
+    public Delete_print_doc() {
+   String host="jdbc:mysql://localhost:3306/requestdb?zeroDateTimeBehavior=convertToNull";
+        String uName="root";
+        String uPass="";
+        String deleteqry="delete from documentprincipalacceptdb "+ "WHERE id=?";
+        PreparedStatement ps1=null;
+        try {
+                      Class.forName("com.mysql.jdbc.Driver");
+                      Connection con=DriverManager.getConnection(host,uName,uPass);
+                      ps1=con.prepareStatement(deleteqry);
+                      ps1.setInt(1,Student_login.vid_num);
+                      ps1.executeUpdate();
+                         con.close();
+                     
+                      
+                    }
+            catch (Exception er ) {
+            er.printStackTrace();
+            } 
+    }
+    
+}
+
